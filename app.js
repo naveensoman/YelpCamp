a27d26e6862d8e74bad1823a70dbfcce7d098502
Naveen Soman <naveensoman@gmail.com>
2017-04-28 19:23:48 +0000

    db url as env variable added

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ var express = require("express"),
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
-
+console.log(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost/yelp_camp_v13");
-mongoose.connect("mongodb://naveensoman:naveen1510@ds123311.mlab.com:23311/yelpcampnaveen");
+// mongoose.connect("mongodb://naveensoman:naveen1510@ds123311.mlab.com:23311/yelpcampnaveen");
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
